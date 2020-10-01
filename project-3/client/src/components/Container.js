0655a2e8870918fa6d625c67e004e9d11df6cf46
LM&T <tbbragg83@gmail.com>
2020-10-01 15:24:55 -0500

    adding unstyled nav and empty pages

---
+++ project-3/client/src/components/Container.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import NavTabs from "./NavTabs";
+import Home from "./pages/Home";
+import Search from "./pages/Search";
+import Contact from "./pages/Contact";
+
+class Container extends Component {
+  state = {
+    currentPage: "Home"
+  };
+
+  handlePageChange = page => {
+    this.setState({ currentPage: page });
+  };
+
+  renderPage = () => {
+    if (this.state.currentPage === "Home") {
+      return <Home />;
+    } else if (this.state.currentPage === "Search") {
+      return <Search />;
+    } else {
+      return <Contact />;
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <NavTabs
+          currentPage={this.state.currentPage}
+          handlePageChange={this.handlePageChange}
+        />
+        {this.renderPage()}
+      </div>
+    );
+  }
+}
+
+export default Container;
---
+++ project-3/client/src/components/NavTabs.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+function NavTabs(props) {
+  return (
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <a
+          href="#home"
+          onClick={() => props.handlePageChange("Home")}
+          className={props.currentPage === "Home" ? "nav-link active" : "nav-link"}
+        >
+          Home
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#search"
+          onClick={() => props.handlePageChange("Search")}
+          className={props.currentPage === "Search" ? "nav-link active" : "nav-link"}
+        >
+          Search
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#contact"
+          onClick={() => props.handlePageChange("Contact")}
+          className={props.currentPage === "Contact" ? "nav-link active" : "nav-link"}
+        >
+          Contact
+        </a>
+      </li>
+    </ul>
+  );
+}
+
+export default NavTabs;
