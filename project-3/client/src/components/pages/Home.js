4672661fb432b97420a24481178d01680e05bafc
demi0504 <ripley.demi@gmail.com>
2020-10-01 16:35:05 -0500

    bulma cdn

--- project-3/client/src/components/pages/Home.js
+++ project-3/client/src/components/pages/Home.js
@@ -2,7 +2,12 @@ import React from "react";
 
 const Home = () => (
   <div>
-    <h1>Home Page</h1>
+    <div className="columns">
+      <div className="column">Worry Free Pet Boarding.</div>
+      <div className="column">
+        <img src="https://s3.amazonaws.com/petcentral.com/wp-content/uploads/2019/07/22160617/dog-hotel-article.jpg" alt="dog in hotel"/>
+      </div>
+    </div>
     
   </div>
 );
