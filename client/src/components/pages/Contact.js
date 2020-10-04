ecf4141be06c79888c94cd39b9996014361b0536
LM&T <tbbragg83@gmail.com>
2020-10-04 02:36:29 -0500

    added state/input handling/and form submit to contact page/removed console logs from ThreeImage.js

--- client/src/components/pages/Contact.js
+++ client/src/components/pages/Contact.js
@@ -1,34 +1,88 @@
-import React from "react";
+import React, { Component } from "react";
 import OneImage from "../OneImage"
 import ThreeImage from "../ThreeImage"
 
-const Contact = () => (
-  <div>
-    <h1>Contact Us!</h1>
-    <OneImage />
-    <section className="section">
-      <div className="container">
-        <div className="field">
-          <div className="control">
-            <input className="input is-primary" type="text" placeholder="Name" />
-          </div>
-        </div>
-        <div className="field">
-          <div className="control">
-            <input className="input is-primary" type="text" placeholder="Email" />
-          </div>
-        </div>
+class Contact extends Component {
+  state = {
+    fullName: "",
+    Email: "",
+    Message: ""
+  };
 
-        <div className="control">
-          <textarea className="textarea has-fixed-size" placeholder="Message"></textarea>
-        </div>
-        <div className="control">
-          <button className="button is-primary">Submit</button>
-        </div>
+  handleInputChange = event => {
+    const { name, value } = event.target;
+
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+    //Should change alert to modal
+    alert(`${this.state.fullName}, message sent!`);
+    this.setState({
+      fullName: "",
+      Email: "",
+      Message: ""
+    });
+  };
+  
+  
+  render() {
+    return (
+      <div>
+        <h1>Contact Us!</h1>
+        <OneImage />
+        <section className="section">
+          <div className="container">
+            <div className="field">
+              <div className="control">
+                <input 
+                className="input is-primary" 
+                type="text" 
+                placeholder="Name" 
+                value={this.state.fullName}
+                name="fullName"
+                onChange={this.handleInputChange}
+                />
+              </div>
+            </div>
+            <div className="field">
+              <div className="control">
+                <input 
+                className="input is-primary" 
+                type="text" 
+                placeholder="Email" 
+                value={this.state.Email}
+                name="Email"
+                onChange={this.handleInputChange}
+                />
+              </div>
+            </div>
+
+            <div className="control">
+              <textarea 
+                className="textarea has-fixed-size" 
+                placeholder="Message"
+                value={this.state.Message}
+                name="Message"
+                onChange={this.handleInputChange}
+              ></textarea>
+            </div>
+            
+            <div className="control">
+              <button 
+              className="button is-primary"
+              onClick={this.handleFormSubmit}
+              >Submit</button>
+            </div>
+          </div>
+        </section>
+        <ThreeImage />
       </div>
-    </section>
-    <ThreeImage />
-  </div>
-);
+    );
+  }
+};
 
 export default Contact;
