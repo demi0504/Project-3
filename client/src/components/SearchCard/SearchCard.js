6f475cf9d67f3204d55c41767cc55210975ec5e3
LM&T <tbbragg83@gmail.com>
2020-10-06 00:09:46 -0500

    Filling out Search Card

--- client/src/components/SearchCard/SearchCard.js
+++ client/src/components/SearchCard/SearchCard.js
@@ -2,11 +2,23 @@ import React from "react";
 
 function SearchCard(props) {
   return (
-    <div>
-
-      <p>This is Working!</p>
-
+    
+    <div class="card">
+      <div class="column is-6 card-image">
+        <figure class="image">
+          <img src={props.image} alt="Placeholder" />
+        </figure>
+      </div>
+      <div class="column is-6">
+        <div class="media-content">
+          <p class="title is-4">{props.name}Title</p>
+        </div>
+        <div class="content">
+          {props.description} Description
+        </div>
+      </div>
     </div>
+
   );
 }
 
