3241ac2fa7226b8ee0b5e0e072eb00744a6a0527
Billy Hicks <billy_jerome_jr@me.com>
2020-10-05 22:14:36 -0500

    meat of styling is on page, need to figure out check out bug

--- client/src/components/pages/Search.js
+++ client/src/components/pages/Search.js
@@ -20,63 +20,68 @@ class Search extends Component {
     return (
       <div className="container">
         <p className="column is-5 is-offset-1 thick"><strong>Bring out ye dogs</strong></p>
-          <h1 className="column is-5 is-offset-2 thick">What are the dates of your stay?</h1>
-        <div className="section">
-          <Columns>
-            <label className="column is-offset-one-fifth" for="start">Check In</label>
-                <input className="column" type="date" id="start" name="trip-start" value="2020-10-1" min="2020-01-01" max="2021-12-31"/>
-                
-              
-            <label for="start" className="column is-offset-one-fifth">Check Out</label>
-              <input className="column" type="date" id="start" name="trip-start" value="2020-10-2" min="2020-01-01" max="2021-12-31"/>
+          <Container>
+            <div className="section">
+            <Columns>
+              <Columns.Column>
+                <h1 className="column is-5 is-offset-1">What are the dates of your stay?</h1>
+                  <div class="field">
+                    <label className="label column is-offset-1" for="start">Check In</label>
+                    <input className="column is-offset-1" type="date" id="start" name="trip-start" value="2020-10-1" min="2020-01-01" max="2021-12-31"/>
+                  </div>
+              </Columns.Column>
+              <Columns.Column>
+                <div class="field">
+                  <label className="label" for="start">Check Out</label>
+                  <input className="column" type="date" id="start" name="trip-start" value="2020-10-1" min="2020-01-01" max="2021-12-31"/>
+                </div>
+              </Columns.Column>
             </Columns>
-          </div>
+            </div>
+          </Container>
+          
             <h1 className="column is-5 is-offset-1"><strong>How many dogs need boarding</strong></h1>
               <br/>
-                <div className={"dropdown" + (this.state.collapsed ? "" : " is-active")} tabIndex="0" onBlur={() => this.handleToggle()}>
-                  <div className="dropdown-trigger">
-                      <button className="button" aria-haspopup="true" aria-controls="dropdown-menu" onClick={() => this.handleToggle()}>
-                          <span>Number of Dogs</span>
-                          <span className="icon is-small">
-                              <i className="fas fa-angle-down" aria-hidden="true"></i>
-                          </span>
-                      </button>
+                <div className="column is-5 is-offset-1">  
+                  <div className="select is-rounded">
+                    <select>
+                      <option>0</option>
+                      <option>1</option>
+                      <option>2</option>
+                      <option>3</option>
+                      <option>4</option>
+                      <option>5</option>
+                    </select>
                   </div>
-                  <div className="dropdown-menu" id="dropdown-menu" role="menu">
-                      <div className="dropdown-content">
-                          <a href="#" className="dropdown-item">1</a>
-                          <a href="#" className="dropdown-item">2</a>
-                          <a href="#" className="dropdown-item">3</a>
-                          <a href="#" className="dropdown-item">4</a>
-                          <a href="#" className="dropdown-item">5</a>
-                      </div>
-                  </div>
-              </div>
-            <h1><strong>Does your dog require grooming services?</strong></h1>
-            <br/>
-              <div className="control">
-                <label className="radio">
-                  <input type="radio" name="answer"/>
-                  Yes
-                </label>
-                <label className="radio">
-                  <input type="radio" name="answer"/>
-                  No
-                </label>
-              </div>  
-          
-          <hr/>
-          <h1><strong>Boarders in Nashville</strong></h1>
-            <div class="field has-addons">
-              <div class="control">
-                <input class="input" type="text" placeholder="Find a Boarder"/>
-                  </div>
-                <div class="control">
-                  <a class="button is-info">
-                    Search
-                  </a>
                 </div>
+            <h1 className="column is-5 is-offset-1"><strong>Does your dog require grooming services?</strong></h1>
+            <br/>
+              <div className="column is-5 is-offset-1">
+                <div className="control">
+                  <label className="radio">
+                    <input type="radio" name="answer"/>
+                    Yes
+                  </label>
+                  <label className="radio">
+                    <input type="radio" name="answer"/>
+                    No
+                  </label>
+                </div>  
               </div>
+          <hr/>
+          <h1 className="column is-5 is-offset-1"><strong>Boarders in Nashville</strong></h1>
+            <div className="column is-5 is-offset-1">
+              <div class="field has-addons">
+                <div class="control">
+                  <input class="input" type="text" placeholder="Find a Boarder"/>
+                    </div>
+                  <div class="control">
+                    <a class="button is-info">
+                      Search
+                    </a>
+                  </div>
+                </div>
+            </div>
           </div>
         );
                 }
