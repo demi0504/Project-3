4027b2322d46575b493f20a8441ccaec95ba9232
Kyle <kjknudtson1989@gmail.com>
2020-10-07 16:44:40 -0500

    Added to facility model and changed contact model to user model

--- models/facility.js
+++ models/facility.js
@@ -4,6 +4,9 @@ const Schema = mongoose.Schema;
 const facilitySchema = new Schema({
     name: { type: String, required: true },
     address: { type: String, required: true },
+    city: { type: String, required: true },
+    state: { type: String, required: true },
+    zipcode: { type: Number, required: true },
     phone: { type: String, required: true },
     occupancy: { type: Number, required: true },
     daycare: { type: Boolean },
