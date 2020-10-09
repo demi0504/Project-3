72eef233f04cd6a508e3514909dbd004d6343a75
demi0504 <ripley.demi@gmail.com>
2020-10-08 22:40:17 -0500

    that contact form is going places yall

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,11 +1,15 @@
 const router = require("express").Router();
-const userRoutes = require("./users");
+// const userRoutes = require("./users");
 const facilityRoutes = require("./facilities");
+const sendMailRoutes = require("./sendMail.js");
 
 // Facility routes
 router.use("/facilities", facilityRoutes);
 
 // User routes
-router.use("/user", userRoutes);
+// router.use("/user", userRoutes);
+
+//SendMail routes
+router.post("/api/contact-form", sendMailRoutes);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require("express");
-
+const bodyParser = require('body-parser');
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
+const cors = require('cors');
 const PORT = process.env.PORT || 3001;
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
