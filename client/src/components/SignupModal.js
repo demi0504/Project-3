bdb3a5d921a4244a0c845e0b73966e3b1e7c0b60
demi0504 <ripley.demi@gmail.com>
2020-10-08 23:55:29 -0500

    a lil bit of styling on contact. tried to get that reset form working

--- client/src/components/SignupModal.js
+++ client/src/components/SignupModal.js
@@ -1,231 +1,231 @@
-import React, { useState } from "react";
-import API from "../../utils/API";
-const Modal = ({ closeModal, modalState }) => {
-  if(!modalState) {
-    return null;
-  }
+// import React, { useState } from "react";
+// import API from "../../utils/API";
+// const Modal = ({ closeModal, modalState }) => {
+//   if(!modalState) {
+//     return null;
+//   }
   
-  const [facilities, setFacilities] = useState([])
-  const [formObject, setFormObject] = useState({
-    name: "",
-    address: "",
-    city: "",
-    state: "",
-    zip: "",
-    phone: "",
-    about: "",
-    safety: "",
-    requirements: "",
-    src: "",
-    occupancy: "",
-    daycare: "",
-    boarding: "",
-    grooming: ""
-  })
+//   // const [facilities, setFacilities] = useState([])
+//   // const [formObject, setFormObject] = useState({
+//   //   name: "",
+//   //   address: "",
+//   //   city: "",
+//   //   state: "",
+//   //   zip: "",
+//   //   phone: "",
+//   //   about: "",
+//   //   safety: "",
+//   //   requirements: "",
+//   //   src: "",
+//   //   occupancy: "",
+//   //   daycare: "",
+//   //   boarding: "",
+//   //   grooming: ""
+//   // })
 
-  function handleInputChange(event) {
-    const { name, value } = event.target;
-    setFormObject({...formObject, [name]: value})
-  };
+//   function handleInputChange(event) {
+//     const { name, value } = event.target;
+//     setFormObject({...formObject, [name]: value})
+//   };
 
-  function handleFormSubmit(event) {
-    event.preventDefault();
-    if (formObject.name && formObject.address && formObject.city && formObject.state && formObject.zip && formObject.phone) {
-      API.saveFacility({
-        name: formObject.name,
-        address: formObject.address,
-        city: formObject.city,
-        state: formObject.state,
-        zip: formObject.zip,
-        phone: formObject.phone,
-        about: formObject.about,
-        safety: formObject.safety,
-        requirements: formObject.requirements,
-        src: formObject.src,
-        occupancy: formObject.occupancy,
-        daycare: formObject.daycare,
-        boarding: formObject.boarding,
-        grooming: formObject.grooming
-      })
-        .then(() => setFormObject({
-          name: "",
-          address: "",
-          city: "",
-          state: "",
-          zip: "",
-          phone: "",
-          about: "",
-          safety: "",
-          requirements: "",
-          src: "",
-          occupancy: "",
-          daycare: "",
-          boarding: "",
-          grooming: ""
-        }))
-    } 
-  };
+//   function handleFormSubmit(event) {
+//     event.preventDefault();
+//     if (formObject.name && formObject.address && formObject.city && formObject.state && formObject.zip && formObject.phone) {
+//       API.saveFacility({
+//         name: formObject.name,
+//         address: formObject.address,
+//         city: formObject.city,
+//         state: formObject.state,
+//         zip: formObject.zip,
+//         phone: formObject.phone,
+//         about: formObject.about,
+//         safety: formObject.safety,
+//         requirements: formObject.requirements,
+//         src: formObject.src,
+//         occupancy: formObject.occupancy,
+//         daycare: formObject.daycare,
+//         boarding: formObject.boarding,
+//         grooming: formObject.grooming
+//       })
+//         .then(() => setFormObject({
+//           name: "",
+//           address: "",
+//           city: "",
+//           state: "",
+//           zip: "",
+//           phone: "",
+//           about: "",
+//           safety: "",
+//           requirements: "",
+//           src: "",
+//           occupancy: "",
+//           daycare: "",
+//           boarding: "",
+//           grooming: ""
+//         }))
+//     } 
+//   };
 
-  return(
-    <div className="modal is-active">
-      <div className="modal-background" onClick={closeModal} />
-      <div className="modal-card">
-        <header className="modal-card-head">
-          <p className="modal-card-title">Business Info</p>
-          <button className="delete" onClick={closeModal} />
-        </header>
-        <section className="modal-card-body">
-          <div className="content">
-            <h2>Facility Name</h2>
-            <div className="field">
-              <div className="control">
-                <input className="input is-primary" type="text" placeholder="" />
-              </div>
-            </div>
-            <h2>Address</h2>
-            <div className="field">
-              <div className="control">
-                <input className="input is-primary" type="text" placeholder="Street Number" />
-              </div>
-            </div>
-            <div className="columns">
-              <div className="column">
-                <div className="control">
-                  <input className="input is-primary" type="text" placeholder="City" />
-                </div>
-              </div>
-              <div className="column">
-                <div className="control">
-                  <div className="select is-primary">
-                    <select>
-                      <option>AL</option>
-                      <option>AK</option>
-                      <option>AR</option>
-                      <option>AZ</option>
-                      <option>CA</option>
-                      <option>CO</option>
-                      <option>CT</option>
-                      <option>DE</option>
-                      <option>FL</option>
-                      <option>GA</option>
-                      <option>HI</option>
-                      <option>IA</option>
-                      <option>ID</option>
-                      <option>IL</option>
-                      <option>IN</option>                      
-                      <option>KS</option>
-                      <option>KY</option>
-                      <option>LA</option>
-                      <option>MA</option>
-                      <option>MD</option>
-                      <option>ME</option>                                            
-                      <option>MI</option>
-                      <option>MN</option>
-                      <option>MO</option>
-                      <option>MS</option>                      
-                      <option>MT</option>
-                      <option>NC</option>
-                      <option>ND</option>
-                      <option>NE</option>
-                      <option>NH</option>
-                      <option>NJ</option>
-                      <option>NM</option>
-                      <option>NV</option>                  
-                      <option>NY</option>                      
-                      <option>OH</option>
-                      <option>OK</option>
-                      <option>OR</option>
-                      <option>PA</option>
-                      <option>RI</option>
-                      <option>SC</option>
-                      <option>SD</option>
-                      <option>TN</option>
-                      <option>TX</option>
-                      <option>UT</option>
-                      <option>VA</option>
-                      <option>VT</option>                      
-                      <option>WA</option>
-                      <option>WI</option>
-                      <option>WV</option>                      
-                      <option>WY</option>
-                    </select>
-                  </div>
-                </div>
-              </div>
-              <div className="column">
-                <div className="control">
-                    <input className="input is-primary" type="text" placeholder="Zip Code" />
-                </div>
-              </div>
-            </div>
-            <h2>Phone</h2>
-            <div className="field">
-              <div className="control">
-                <input className="input is-primary" type="text" placeholder="Ex: 615-997-4323" />
-              </div>
-            </div>
-            <h2>Max Occupancy</h2>
-            <div className="field">
-              <div className="control">
-                <input className="input is-primary" type="text" placeholder="Ex: 20" />
-              </div>
-            </div>
-            <h2>Give a brief description of your business:</h2>
-            <div className="control">
-              <textarea className="textarea has-fixed-size is-primary" placeholder=""></textarea>
-            </div>
-            <h2>Do you have any requirements?</h2>
-            <div className="control">
-              <textarea className="textarea has-fixed-size is-primary" placeholder="Ex: Must have up to date vet records."></textarea>
-            </div>
-            <h2>Do you have a website?</h2>
-            <div className="field">
-              <div className="control">
-                <input className="input is-primary" type="text" placeholder="" />
-              </div>
-            </div>
-            <h2>Do you offer day care services?</h2>
-            <div className="control">
-                <label className="radio">
-                    <input type="radio" value="true" name="answer" />
-                    Yes
-                </label>
-                <label className="radio">
-                    <input type="radio" value="false" name="answer" />
-                    No
-                </label>
-            </div>
-            <h2>Do you offer boarding services?</h2>
-            <div className="control">
-                <label className="radio">
-                    <input type="radio" value="true" name="answer2" />
-                    Yes
-                </label>
-                <label className="radio">
-                    <input type="radio" value="false" name="answer2" />
-                    No
-                </label>
-            </div>
-            <h2>Do you offer grooming services?</h2>
-            <div className="control">
-                <label className="radio">
-                    <input type="radio" value="true" name="answer3" />
-                    Yes
-                </label>
-                <label className="radio">
-                    <input type="radio" value="false" name="answer3" />
-                    No
-                </label>
-            </div>
-          </div>
-        </section>
-        <footer className="modal-card-foot">
-          <button className="button is-success">Submit</button>
-          <a className="button" onClick={closeModal}>Cancel</a>
-        </footer>
-      </div>
-    </div>
-  );
-}
+//   return(
+//     <div className="modal is-active">
+//       <div className="modal-background" onClick={closeModal} />
+//       <div className="modal-card">
+//         <header className="modal-card-head">
+//           <p className="modal-card-title">Business Info</p>
+//           <button className="delete" onClick={closeModal} />
+//         </header>
+//         <section className="modal-card-body">
+//           <div className="content">
+//             <h2>Facility Name</h2>
+//             <div className="field">
+//               <div className="control">
+//                 <input className="input is-primary" type="text" placeholder="" />
+//               </div>
+//             </div>
+//             <h2>Address</h2>
+//             <div className="field">
+//               <div className="control">
+//                 <input className="input is-primary" type="text" placeholder="Street Number" />
+//               </div>
+//             </div>
+//             <div className="columns">
+//               <div className="column">
+//                 <div className="control">
+//                   <input className="input is-primary" type="text" placeholder="City" />
+//                 </div>
+//               </div>
+//               <div className="column">
+//                 <div className="control">
+//                   <div className="select is-primary">
+//                     <select>
+//                       <option>AL</option>
+//                       <option>AK</option>
+//                       <option>AR</option>
+//                       <option>AZ</option>
+//                       <option>CA</option>
+//                       <option>CO</option>
+//                       <option>CT</option>
+//                       <option>DE</option>
+//                       <option>FL</option>
+//                       <option>GA</option>
+//                       <option>HI</option>
+//                       <option>IA</option>
+//                       <option>ID</option>
+//                       <option>IL</option>
+//                       <option>IN</option>                      
+//                       <option>KS</option>
+//                       <option>KY</option>
+//                       <option>LA</option>
+//                       <option>MA</option>
+//                       <option>MD</option>
+//                       <option>ME</option>                                            
+//                       <option>MI</option>
+//                       <option>MN</option>
+//                       <option>MO</option>
+//                       <option>MS</option>                      
+//                       <option>MT</option>
+//                       <option>NC</option>
+//                       <option>ND</option>
+//                       <option>NE</option>
+//                       <option>NH</option>
+//                       <option>NJ</option>
+//                       <option>NM</option>
+//                       <option>NV</option>                  
+//                       <option>NY</option>                      
+//                       <option>OH</option>
+//                       <option>OK</option>
+//                       <option>OR</option>
+//                       <option>PA</option>
+//                       <option>RI</option>
+//                       <option>SC</option>
+//                       <option>SD</option>
+//                       <option>TN</option>
+//                       <option>TX</option>
+//                       <option>UT</option>
+//                       <option>VA</option>
+//                       <option>VT</option>                      
+//                       <option>WA</option>
+//                       <option>WI</option>
+//                       <option>WV</option>                      
+//                       <option>WY</option>
+//                     </select>
+//                   </div>
+//                 </div>
+//               </div>
+//               <div className="column">
+//                 <div className="control">
+//                     <input className="input is-primary" type="text" placeholder="Zip Code" />
+//                 </div>
+//               </div>
+//             </div>
+//             <h2>Phone</h2>
+//             <div className="field">
+//               <div className="control">
+//                 <input className="input is-primary" type="text" placeholder="Ex: 615-997-4323" />
+//               </div>
+//             </div>
+//             <h2>Max Occupancy</h2>
+//             <div className="field">
+//               <div className="control">
+//                 <input className="input is-primary" type="text" placeholder="Ex: 20" />
+//               </div>
+//             </div>
+//             <h2>Give a brief description of your business:</h2>
+//             <div className="control">
+//               <textarea className="textarea has-fixed-size is-primary" placeholder=""></textarea>
+//             </div>
+//             <h2>Do you have any requirements?</h2>
+//             <div className="control">
+//               <textarea className="textarea has-fixed-size is-primary" placeholder="Ex: Must have up to date vet records."></textarea>
+//             </div>
+//             <h2>Do you have a website?</h2>
+//             <div className="field">
+//               <div className="control">
+//                 <input className="input is-primary" type="text" placeholder="" />
+//               </div>
+//             </div>
+//             <h2>Do you offer day care services?</h2>
+//             <div className="control">
+//                 <label className="radio">
+//                     <input type="radio" value="true" name="answer" />
+//                     Yes
+//                 </label>
+//                 <label className="radio">
+//                     <input type="radio" value="false" name="answer" />
+//                     No
+//                 </label>
+//             </div>
+//             <h2>Do you offer boarding services?</h2>
+//             <div className="control">
+//                 <label className="radio">
+//                     <input type="radio" value="true" name="answer2" />
+//                     Yes
+//                 </label>
+//                 <label className="radio">
+//                     <input type="radio" value="false" name="answer2" />
+//                     No
+//                 </label>
+//             </div>
+//             <h2>Do you offer grooming services?</h2>
+//             <div className="control">
+//                 <label className="radio">
+//                     <input type="radio" value="true" name="answer3" />
+//                     Yes
+//                 </label>
+//                 <label className="radio">
+//                     <input type="radio" value="false" name="answer3" />
+//                     No
+//                 </label>
+//             </div>
+//           </div>
+//         </section>
+//         <footer className="modal-card-foot">
+//           <button className="button is-success">Submit</button>
+//           <a className="button" onClick={closeModal}>Cancel</a>
+//         </footer>
+//       </div>
+//     </div>
+//   );
+// }
 
-export default Modal;
+// export default Modal;
--- client/src/components/pages/Contact.js
+++ client/src/components/pages/Contact.js
@@ -4,6 +4,7 @@ import OneImage from "../OneImage"
 import ThreeImage from "../ThreeImage"
 import ContactAlertModal from "../ContactAlertModal";
 import DogSVG from "../DogSVG/DogSVG"
+import { Columns, Container } from 'react-bulma-components'
 
 class Contact extends Component {
   constructor(props) {
@@ -33,6 +34,10 @@ class Contact extends Component {
     })
   }
 
+  resetForm(){
+    document.getElementById('contact-form').reset();
+  }
+
   openModal() {    
     this.setState((prev, props) => {
       const newState = !prev.modalState;
@@ -45,60 +50,66 @@ class Contact extends Component {
     return (
       <div>
         <DogSVG />
-        <h1>Contact Us!</h1>
-        <OneImage />
         <section className="section">
-          <div className="container">
-            <form id="contact-form" onSubmit={this.handleFormSubmit.bind(this)} method="POST">
-            <div className="field">
-              <div className="control">
-                <input 
-                className="input is-primary" 
-                type="text" 
-                placeholder="Name" 
-                value={this.state.name}
-                name="name"
-                onChange={this.handleInputChange.bind(this)}
-                />
-              </div>
-            </div>
-            <div className="field">
-              <div className="control">
-                <input 
-                className="input is-primary" 
-                type="text" 
-                placeholder="Email" 
-                value={this.state.email}
-                name="email"
-                onChange={this.handleInputChange.bind(this)}
-                />
-              </div>
-            </div>
-
-            <div className="control">
-              <textarea 
-                className="textarea has-fixed-size" 
-                placeholder="Message"
-                value={this.state.message}
-                name="message"
-                onChange={this.handleInputChange.bind(this)}
-              ></textarea>
-            </div>
-            
-            <div className="control">
-              <button className="button is-primary">Submit</button>
-            </div>
-            <ContactAlertModal 
-            closeModal={this.openModal} 
-            modalState={this.state.modalState} 
-            >
-              {this.state.fullName}
-            </ContactAlertModal>
-            {this.state.emailSent === true && <p className="d-inline success-msg">Email Sent</p>}
-            {this.state.emailSent === false && <p className="d-inline err-msg">Email Not Sent</p>}
-            </form>
-          </div>
+          <Container>
+            <Columns>
+              <Columns.Column className="is-half">
+                <h1 className="thick">Lets Talk!</h1>
+                <form id="contact-form" onSubmit={this.handleFormSubmit.bind(this)} method="POST">
+                <div className="field">
+                  <div className="control">
+                    <input 
+                    className="input is-primary" 
+                    type="text" 
+                    placeholder="Name" 
+                    value={this.state.name}
+                    name="name"
+                    onChange={this.handleInputChange.bind(this)}
+                    />
+                  </div>
+                </div>
+                <div className="field">
+                  <div className="control">
+                    <input 
+                    className="input is-primary" 
+                    type="text" 
+                    placeholder="Email" 
+                    value={this.state.email}
+                    name="email"
+                    onChange={this.handleInputChange.bind(this)}
+                    />
+                  </div>
+                  <p class="help">Don't worry, we won't share your email with anyone.</p>
+                </div>
+                <div className="control">
+                  <textarea 
+                    className="textarea has-fixed-size is-primary" 
+                    placeholder="Message"
+                    value={this.state.message}
+                    name="message"
+                    onChange={this.handleInputChange.bind(this)}
+                  ></textarea>
+                </div>
+                <div className="control">
+                  <button className="button is-primary contact-btn">Submit</button>
+                </div>
+                <ContactAlertModal 
+                closeModal={this.openModal} 
+                modalState={this.state.modalState} 
+                >
+                  {this.state.fullName}
+                </ContactAlertModal>
+                {this.state.emailSent === true && <p className="d-inline success-msg">Email Sent</p>}
+                {this.state.emailSent === false && <p className="d-inline err-msg">Email Not Sent</p>}
+                </form>
+              </Columns.Column>
+              <Columns.Column>
+              <img className="is-transparent" src="https://images.dog.ceo/breeds/poodle-standard/n02113799_2292.jpg" alt="standard poodle with a bubble"/>
+              </Columns.Column>
+            </Columns>
+          </Container>
         </section>
+        <OneImage />
         <ThreeImage />
       </div>
     );
