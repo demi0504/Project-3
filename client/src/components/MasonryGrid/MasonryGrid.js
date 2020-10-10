579119df63a6eb2a3597f27d3ca138ab9dd9f82c
LM&T <tbbragg83@gmail.com>
2020-10-10 18:09:47 -0500

    adding masonry grid component

---
+++ client/src/components/MasonryGrid/MasonryGrid.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Masonry from 'react-masonry-css'
+import OneImage from "../OneImage"
+import "./masonry.css";
+
+function MasonryGrid() {
+    // const [items, set] = useState(data)
+    // useEffect(() => void setInterval(() => set(shuffle), 8000), [])
+
+    return (
+        <div className="container transp">
+            <div className="masonry-container transp">
+                <Masonry className="my-masonry-grid"
+                            breakpointCols={4}
+                            columnClassName="my-masonry-grid_column" 
+                            brakePoints={1}>
+                    <OneImage />
+                    <OneImage />
+                    <OneImage />
+                    <OneImage />
+                    <OneImage />
+                    <OneImage />
+                    <OneImage />
+                    <OneImage />
+                    <OneImage />
+                    <OneImage />
+                    <OneImage />
+                    <OneImage />
+                </Masonry>
+            </div>
+            
+        </div>
+    )
+    
+}
+
+
+export default MasonryGrid;
