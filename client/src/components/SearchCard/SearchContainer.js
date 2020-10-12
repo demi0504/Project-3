77bbab9e73220fcb325ddf2d0bf545057755bc2b
LM&T <tbbragg83@gmail.com>
2020-10-12 02:37:15 -0500

    still working on getting info out of database and onto component

--- client/src/components/SearchCard/SearchContainer.js
+++ client/src/components/SearchCard/SearchContainer.js
@@ -1,23 +1,35 @@
 import React, { Component } from "react";
 import SearchCard from "./SearchCard";
-// import businesses from "";
+import API from "../../utils/API";
 
 class SearchContainer extends Component {
   state = {
-    // businesses
+    businesses: "",
+    filteredBusinesses: ""
   };
+
+  componentDidMount() {
+    API.getFacilities()
+      .then(res => 
+        {console.log("Working!!" + res.data)
+          this.setState({ 
+          businesses: res.data,
+          filteredBusinesses: res.data })})
+          
+      .catch(err => console.log(err));
+  };
+
 
   render() {
     return (
 
       <div>
-            {/* {this.state.businesses.map(biz => ( */}
+            {/* {this.state.businesses.map(biz => (
           <SearchCard
-            // id={biz.id}
-            // key={biz.id}
-
+            id={biz.id}
+            key={biz.id}
           />
-        {/* ))} */}
+         ))} */}
       </div>
 
     );
