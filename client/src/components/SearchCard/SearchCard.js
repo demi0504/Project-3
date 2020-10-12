e7a4b7d81c1c5e9c44d4b05f5bd286a2730d3357
Kyle <kjknudtson1989@gmail.com>
2020-10-11 21:47:11 -0500

    Added more info to searchCard component

--- client/src/components/SearchCard/SearchCard.js
+++ client/src/components/SearchCard/SearchCard.js
@@ -3,19 +3,22 @@ import React from "react";
 function SearchCard(props) {
   return (
     
-    <div class="card">
-      <div class="column is-6 card-image">
-        <figure class="image">
-          <img src={props.image} alt="Placeholder" />
-        </figure>
-      </div>
-      <div class="column is-6">
-        <div class="media-content">
-          <p class="title is-4">{props.name}Title</p>
+    <div className="card">
+      <div className="column is-12">
+        <div className="media-content">
+          <p className="title is-4">{props.name}</p>
         </div>
-        <div class="content">
-          {props.description} Description
+        <div className="media-content">
+          <p className="title is-4">{props.address}</p>
+          <p className="title is-4">{props.city}, {props.state} {props.zip}</p>
         </div>
+        <div className="media-content">
+          <p className="title is-4">{props.phone}</p>
+        </div>
+        <div className="content">
+          {props.about}
+        </div>
+        <button className="button is-light">More Info</button>
       </div>
     </div>
 
