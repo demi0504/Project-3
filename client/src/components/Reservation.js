98e86c75324867d87aea8c828fdf1a7808d255c6
demi0504 <ripley.demi@gmail.com>
2020-10-13 17:48:04 -0500

    some backend done for reso.

--- client/src/components/Reservation.js
+++ client/src/components/Reservation.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Columns, Container } from 'react-bulma-components'
 import { Button } from "react-bulma-components";
 import Calendar from 'react-calendar';
+import Axios from 'axios';
 import 'react-calendar/dist/Calendar.css';
 
 class Reservation extends Component {
@@ -22,6 +23,23 @@ class Reservation extends Component {
 
 }
 
+  handleReservation(event) {
+    Axios.post('http://127.0.0.1:3001/api/reservation/create', null,
+      {
+        params: {
+          dateStart: this.state.date[0],
+          dateEnd: this.state.date[1]
+        }
+      })
+      .then(res => {
+        console.log(res)
+    })
+    .catch(error => {
+        this.setState({ date: null })
+        console.table(error);
+    })
+  }
+
     render() {
         return (
           <div>
@@ -30,6 +48,7 @@ class Reservation extends Component {
             value={this.state.date}
             selectRange
             />
+             <Button className="reserve-button" onClick={() => this.handleReservation()}>Reserve</Button>
             </div>
         )
     }
