37c50f11b99297d1379f511b5fbc766e8d6a28a9
LM&T <tbbragg83@gmail.com>
2020-10-15 04:33:50 -0500

    commiting AuthContext and AuthService files/react context-Api and hooks not providing state globally. Still needs work

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import { AuthContext } from "./Context/AuthContext"
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Search from './components/pages/Search';
 import Home from './components/pages/Home';
@@ -10,6 +11,10 @@ import Reservation from './components/Reservation';
 
 
 function App() {
+  // const { user, serUser, isAuthenticated, setIsAuthenticated } = useContext(AuthContext);
+  // console.log(user);
+  // console.log(isAuthenticated);
+
   return (
     <Router>
       <div>
