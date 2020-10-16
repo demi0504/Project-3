a2103c569dcd35b4f720d79a77262363e1df748b
LM&T <tbbragg83@gmail.com>
2020-10-16 14:08:07 -0500

    cleaning up commented out code, console logs and unused code

--- models/user.js
+++ models/user.js
@@ -5,7 +5,6 @@ const bcrypt = require("bcrypt")
 const userSchema = new Schema({
   username: { type: String, required: true },
   email: { type: String, required: true },
-  // owner: { type: Boolean, required: true },
   password: { type: String, required: true },
   role: { type: String, enum: ["user", "admin"], required: true },
   facilities : [{type : mongoose.Schema.Types.ObjectId, ref: 'Facility'}]
