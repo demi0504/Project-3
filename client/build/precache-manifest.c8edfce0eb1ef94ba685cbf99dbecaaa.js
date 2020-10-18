0aa54863979267417780ae9bcb45f650780ae311
Billy Hicks <billy_jerome_jr@me.com>
2020-10-18 17:27:13 -0500

    pushing to heroku

---
+++ client/build/precache-manifest.c8edfce0eb1ef94ba685cbf99dbecaaa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "778337f6c1d436cfddfe1b8dfe1e3c90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5bfa4604119b56e133ae",
+    "url": "/static/css/2.1af9eb55.chunk.css"
+  },
+  {
+    "revision": "bf9fdea7024fa4e8e0f3",
+    "url": "/static/css/main.d9d6e1f4.chunk.css"
+  },
+  {
+    "revision": "5bfa4604119b56e133ae",
+    "url": "/static/js/2.93d47dde.chunk.js"
+  },
+  {
+    "revision": "6c5833db93cd7c0f813219a8bc75480b",
+    "url": "/static/js/2.93d47dde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf9fdea7024fa4e8e0f3",
+    "url": "/static/js/main.bb81422c.chunk.js"
+  },
+  {
+    "revision": "53d69f4a0c140d0ca5b5",
+    "url": "/static/js/runtime-main.f2447cd3.js"
+  }
+]);
