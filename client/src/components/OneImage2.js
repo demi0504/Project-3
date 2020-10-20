53a8f228819c2a5b0713d50012f8547ebbb697c8
LM&T <tbbragg83@gmail.com>
2020-10-20 02:54:34 -0500

    masonry styles fix

---
+++ client/src/components/OneImage2.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import OneAPI from "../utils/OneAPI";
+
+
+class OneImage extends Component {
+  state = {
+    result: {}
+  };
+
+  componentDidMount() {
+    this.searchDog();
+  }
+
+  searchDog = query => {
+    OneAPI.findOneDog(query)
+      .then(res =>
+        {this.setState({ result: res.data.message })})
+      .catch(err => console.log(err));
+  };
+
+  
+
+  render() {
+    return (
+      <div className="is-horizontal-center">
+        <img alt={this.state.result} src={this.state.result} className="image-dog"/>
+      </div>
+    );
+  }
+}
+
+
+export default OneImage;
